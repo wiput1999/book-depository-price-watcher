ecf63d42c038417b5e6dd36e58976703476edf4c
Wiput Pootong <wiput.pootong@gmail.com>
2022-02-20 03:48:01 +0700

    remove: unused dep

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,18 @@ import { parse } from 'node-html-parser'
   const url =
     'https://www.bookdepository.com/Building-Microservices-Sam-Newman/9781492034025'
 
-  const res = await axios.post(
-    url,
-    {
-      selectCurrency: 'THB',
+  const res = await axios.get(url, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'accept-language': 'en-US,en;q=0.9,th;q=0.8',
     },
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'accept-language': 'en-US,en;q=0.9,th;q=0.8',
-      },
-    }
-  )
+  })
 
   const html = parse(res.data)
 
   const price = html.querySelector('.sale-price')
 
-  console.log(price?.innerText)
+  if (price) {
+    console.log(price.innerText)
+  }
 })()
