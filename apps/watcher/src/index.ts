7bd5f3f0df038a2edcda4df0f7acca082c90764a
Wiput Pootong <wiput.pootong@gmail.com>
2022-04-15 19:39:12 +0700

    feat(watch): fetching logic from db

--- apps/watcher/src/index.ts
+++ apps/watcher/src/index.ts
@@ -1,24 +1,40 @@
 import axios from 'axios'
 import { parse } from 'node-html-parser'
 import currency from 'currency.js'
+import pino from 'pino'
+import { PrismaClient } from 'db'
 
 async function main() {
-  const url =
-    'https://www.bookdepository.com/Building-Microservices-Sam-Newman/9781492034025'
+  const logger = pino()
+  const db = new PrismaClient()
 
-  const res = await axios.get(url, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'accept-language': 'en-US,en;q=0.9,th;q=0.8',
-    },
-  })
+  const books = await db.book.findMany()
 
-  const html = parse(res.data)
+  for (const book of books) {
+    const bookLogger = logger.child(book)
 
-  const price = html.querySelector('.sale-price')
+    bookLogger.info(`${book.name}: Fetching`)
+    const url = book.url
 
-  if (price) {
-    console.log(currency(price.innerText).value)
+    const res = await axios.get(url, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'accept-language': 'en-US,en;q=0.9,th;q=0.8',
+      },
+    })
+
+    const html = parse(res.data)
+
+    const priceElement = html.querySelector('.sale-price')
+
+    if (priceElement) {
+      const price = currency(priceElement.innerText).value
+      bookLogger.info(`${book.name}: ${price} THB`)
+
+      continue
+    }
+
+    bookLogger.warn(`${book.name}: Price Not Found`)
   }
 }
 
