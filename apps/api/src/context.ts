1a01e2dea753d9d6e01e48e6c3a536116a9c0204
Wiput Pootong <wiput.pootong@gmail.com>
2022-05-16 20:24:12 +0700

    refactor: remove trpc

--- apps/api/src/context.ts
+++ apps/api/src/context.ts
@@ -1,10 +1,13 @@
 import { inferAsyncReturnType } from '@trpc/server'
 import { CreateFastifyContextOptions } from '@trpc/server/adapters/fastify'
+import { PrismaClient } from 'db'
 
 export function createContext({ req, res }: CreateFastifyContextOptions) {
   const user = { name: req.headers.username ?? 'anonymous' }
 
-  return { req, res, user }
+  const db = new PrismaClient()
+
+  return { req, res, user, db }
 }
 
 export type Context = inferAsyncReturnType<typeof createContext>
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,27 +1,19 @@
 import fastify from 'fastify'
 import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify'
 import { createContext } from './context'
-import { appRouter } from './router'
 
 const server = fastify({
   maxParamLength: 5000,
+  logger: true,
 })
 
 const PORT = process.env.PORT || 3002
 
-server.register(fastifyTRPCPlugin, {
-  prefix: '/trpc',
-  trpcOptions: { router: appRouter, createContext },
-})
 ;(async () => {
   try {
-    await server.listen(PORT, () => {
-      console.log(`Server started at port ${PORT}`)
-    })
+    await server.listen(PORT)
   } catch (err) {
     server.log.error(err)
     process.exit(1)
   }
 })()
-
-export type { AppRouter } from './router'
